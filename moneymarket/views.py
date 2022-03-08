d5539069ea52735e65c681331c01aab3fbc8dc28
Aashka2912 <aashkapatel1998@outlook.com>
2022-03-07 20:59:38 -0600

    Update

--- moneymarket/views.py
+++ moneymarket/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
+
 def index(request):
-    return render(request,'index.html',{})
+    return render(request,'index.html', {})
 
 def about(request):
     return render(request, 'about.html', {})
