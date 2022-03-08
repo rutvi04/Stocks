9ee983d44fc0b1fd5e4ab2a6c0193e7841114210
Aashka2912 <aashkapatel1998@outlook.com>
2022-03-08 15:00:08 -0600

    Update

--- moneymarket/views.py
+++ moneymarket/views.py
@@ -4,7 +4,17 @@ from django.http import HttpResponse, HttpRequest
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html', {})
+    import requests
+    import json
+
+    api_request = requests.get("https://api.polygon.io/v1/open-close/AAPL/2020-10-14?adjusted=true&apiKey=XTRyuTSxOF0KKDoTxdVI5hRSRjGrRB0g")
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api="Error...."
+    #pk_5d448bc5ec78430f9f18a309b283a812
+    #XTRyuTSxOF0KKDoTxdVI5hRSRjGrRB0g
+    return render(request,'index.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
